969b1587d9f8dfffbdd9993b6c3754e5101a53e3
gustavoaz7 <gustavo_azevedo_7@hotmail.com>
2020-03-14 20:31:04 +0100

    Project 4 solution

--- Nand2Tetris 1/04/mult/mult.asm
+++ Nand2Tetris 1/04/mult/mult.asm
@@ -7,3 +7,36 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// for (i=0; i <= R1; i++) {
+//    R[2] = R[2] + R[0]
+// }
+
+@R2
+M=0
+
+@i
+M=0
+
+(LOOP)
+  @i
+  D=M
+  @R1
+  D=M-D
+  @END
+  D;JLE
+
+  @R0
+  D=M
+  @R2
+  M=D+M
+
+  @i
+  M=M+1
+
+  @LOOP
+  0;JMP
+
+  (END)
+  @END
+  0;JMP
